cf6567b793313b1e159cfd8a45ed949c44d39004
jperez10git <jperez10@gmail.com>
2023-05-03 10:45:42 +0200

    v2

--- git230503b/src/git230503b/ClaseGit230503b.java
+++ git230503b/src/git230503b/ClaseGit230503b.java
@@ -3,7 +3,8 @@ package git230503b;
 public class ClaseGit230503b {
 
 	public static void main(String[] args) {
-System.out.println("1");
+		System.out.println("1");
+		System.out.println("2");
 	}
 
 }
